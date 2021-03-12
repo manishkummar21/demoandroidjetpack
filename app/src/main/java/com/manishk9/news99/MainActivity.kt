3a9d8a1ba9d1ba03af5e862c7480153a8c72c7cd
Manish <manish.kummar21@gmail.com>
2021-03-12 18:27:39 +0530

    Initial Setup

---
+++ app/src/main/java/com/manishk9/news99/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.manishk9.news99
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
